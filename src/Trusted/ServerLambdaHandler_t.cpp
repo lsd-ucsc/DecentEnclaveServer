a245cb21d5b0832d4d829e3ffa7745a382bb134d
Haofan Zheng <hzheng6@ucsc.edu>
2023-08-22 18:01:46 -0700

    Added more features to server req handler

--- src/Trusted/ServerLambdaHandler_t.cpp
+++ src/Trusted/ServerLambdaHandler_t.cpp
@@ -5,11 +5,15 @@
 
 
 #include <memory>
+#include <string>
 
 #include <sgx_error.h>
-#include <DecentEnclave/Common/Platform/Print.hpp>
+#include <DecentEnclave/Common/CertStore.hpp>
+#include <DecentEnclave/Common/Logging.hpp>
 #include <DecentEnclave/Trusted/Sgx/ComponentConnection.hpp>
+#include <SimpleJson/SimpleJson.hpp>
 #include <SimpleObjects/Internal/make_unique.hpp>
+#include <SimpleObjects/SimpleObjects.hpp>
 
 #include "AppRequestHandler.hpp"
 
@@ -28,12 +32,41 @@ extern "C" sgx_status_t ecall_decent_server_lambda_handler(
 
 	try
 	{
-		DecentEnclaveServer::HandleAppCertRequest(std::move(sock));
+		auto reqJson = sock->SizedRecvBytes<std::string>();
+		auto req = SimpleJson::LoadStr(reqJson);
+
+		std::string method =
+			req.AsDict()[SimpleObjects::String("method")].AsString().c_str();
+
+		if (method == "req_app_cert")
+		{
+			DecentEnclaveServer::HandleAppCertRequest(std::move(sock));
+		}
+		else if (method == "get_svr_cert")
+		{
+			const auto& params =
+				req.AsDict()[SimpleObjects::String("params")].AsList();
+			if (params.size() != 1)
+			{
+				throw std::runtime_error("Invalid number of parameters.");
+			}
+			std::string keyName = params[0].AsString().c_str();
+
+			SimpleObjects::String certPem =
+				CertStore::GetInstance()[keyName].GetCertBase()->GetPem();
+
+			SimpleObjects::Dict resp;
+			resp[SimpleObjects::String("result")] = certPem;
+			sock->SizedSendBytes(SimpleJson::DumpStr(resp));
+		}
 	}
 	catch(const std::exception& e)
 	{
-		Platform::Print::StrErr(
-			std::string("Decent Server Failed to handle App's certificate request: ") +
+		static auto logger =
+			LoggerFactory::GetLogger("DecentServerLambdaHandler");
+
+		logger.Error(
+			std::string("Decent Server Failed to handle App's request: ") +
 			e.what()
 		);
 	}
